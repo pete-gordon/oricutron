f4c1897c958f663b01aae6bdbf7a4ce6ba4fad2f
pete <pete@petergordon.org.uk>
2010-03-17 21:43:34 +0000

    Adding the messagebox files

---
+++ msgbox_sdl.c
@@ -0,0 +1,287 @@
+/*
+**  Oricutron
+**  Copyright (C) 2009 Peter Gordon
+**
+**  This program is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU General Public License
+**  as published by the Free Software Foundation, version 2
+**  of the License.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+**  SDL based file requester
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#include "system.h"
+#include "6502.h"
+#include "via.h"
+#include "8912.h"
+#include "gui.h"
+#include "disk.h"
+#include "machine.h"
+#include "msgbox.h"
+
+struct msgboxbut
+{
+  int x, y;
+  char *str;
+};
+
+extern SDL_Surface *screen;
+static struct textzone *tz = NULL;
+static struct msgboxbut *btns;
+static int cbtn;
+
+static struct msgboxbut ynbuts[] = { {  2, 5, "  YES   " },
+                                     { 50, 5, "   NO   " },
+                                     { -1,-1, NULL } };
+
+static struct msgboxbut ocbuts[] = { {  2, 5, "   OK   " },
+                                     { 50, 5, " CANCEL " },
+                                     { -1,-1, NULL } };
+
+static struct msgboxbut obuts[]  = { { 26, 5, "   OK   " },
+                                     { -1,-1, NULL } };
+
+SDL_bool init_msgbox( void )
+{
+  tz = alloc_textzone( 80, 120, 60, 8, "Oricutron Request" );
+  if( !tz ) return SDL_FALSE;
+
+  return SDL_TRUE;
+}
+
+void shut_msgbox( void )
+{
+  if( tz ) free_textzone( tz );
+}
+
+// Render the filerequester
+static void msgbox_render( struct machine *oric )
+{
+  int i;
+
+  for( i=0; btns[i].x!=-1; i++ )
+  {
+    if( i==cbtn )
+    {
+      tz->cfc = 1;
+      tz->cbc = 2;
+    } else {
+      tz->cfc = 2;
+      tz->cbc = 5;
+    }
+
+    tzstrpos( tz, btns[i].x, btns[i].y, btns[i].str );
+  }
+
+  if( SDL_MUSTLOCK( screen ) )
+    SDL_LockSurface( screen );
+
+  video_show( oric );
+  draw_textzone( tz );
+
+  if( SDL_MUSTLOCK( screen ) )
+    SDL_UnlockSurface( screen );
+
+  SDL_Flip( screen );
+}
+
+static int msgbox_checkover( int x, int y )
+{
+  int i;
+
+  for( i=0; btns[i].x!=-1; i++ )
+  {
+    if( ( y == btns[i].y ) &&
+        ( x >= btns[i].x ) &&
+        ( x < (btns[i].x+strlen(btns[i].str)) ) )
+      return i;
+  }
+  return -1;
+}
+
+SDL_bool msgbox( struct machine *oric, int type, char *msg )
+{
+  int i, j, k, mx, my, presson;
+  int lines[] = { 2*60, 3*60, 5*60 };
+  char *msg2;
+  SDL_Event event;
+  SDL_bool wasunicode;
+
+  wasunicode = SDL_EnableUNICODE( SDL_TRUE );
+  SDL_EnableKeyRepeat( SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL );
+
+  // Clear any old message
+  for( i=1; i<59; i++ )
+  {
+    for( j=0; j<3; j++ )
+    {
+      tz->fc[lines[j]+i] = 2;
+      tz->bc[lines[j]+i] = 3;
+      tz->tx[lines[j]+i] = 32;
+    }
+  }
+
+  for( i=0; msg[i]; i++ ) if( (msg[i]=='\r')||(msg[i]=='\n') ) break;
+
+  for( j=0, k=30-i/2; j<i; j++, k++ )
+    tz->tx[lines[0]+k] = msg[j];
+  
+  while( (msg[i]=='\r')||(msg[i]=='\n') ) i++;
+  if( msg[i] )
+  {
+    msg2 = &msg[i];
+    i = strlen( msg2 );
+    for( j=0, k=30-i/2; j<i; j++, k++ )
+      tz->tx[lines[1]+k] = msg2[j];
+  }
+
+  switch( type )
+  {
+    case MSGBOX_YES_NO:
+      btns = ynbuts;
+      cbtn = 0;
+      break;
+
+    case MSGBOX_OK_CANCEL:
+      btns = ocbuts;
+      cbtn = 1;
+      break;
+    
+    case MSGBOX_OK:
+      btns = obuts;
+      cbtn = 0;
+      break;
+  }
+
+  msgbox_render( oric );
+
+  presson = -1;
+  for( ;; )
+  {
+    if( !SDL_WaitEvent( &event ) ) 
+    {
+      SDL_EnableUNICODE( wasunicode );
+      SDL_EnableKeyRepeat( wasunicode ? SDL_DEFAULT_REPEAT_DELAY : 0, wasunicode ? SDL_DEFAULT_REPEAT_INTERVAL : 0 );
+      return SDL_FALSE;
+    }
+
+    switch( event.type )
+    {
+      case SDL_MOUSEMOTION:
+        if( presson != -1 ) break;
+        mx = (event.motion.x - tz->x)/8;
+        my = (event.motion.y - tz->y)/12;
+        i = msgbox_checkover( mx, my );
+        if( i == -1 ) break;
+        if( i != cbtn )
+        {
+          cbtn = i;
+          msgbox_render( oric );
+        }
+        break;
+      
+      case SDL_MOUSEBUTTONDOWN:
+        if( event.button.button == SDL_BUTTON_LEFT )
+        {
+          mx = (event.motion.x - tz->x)/8;
+          my = (event.motion.y - tz->y)/12;
+          i = msgbox_checkover( mx, my );
+          if( i == -1 ) break;
+          cbtn = i;
+          presson = i;
+          msgbox_render( oric );
+        }
+        break;
+      
+      case SDL_MOUSEBUTTONUP:
+        if( event.button.button == SDL_BUTTON_LEFT )
+        {
+          if( presson == -1 ) break;
+          mx = (event.motion.x - tz->x)/8;
+          my = (event.motion.y - tz->y)/12;
+          i = msgbox_checkover( mx, my );
+          if( i == presson )
+          {
+            switch( type )
+            {
+              case MSGBOX_YES_NO:
+              case MSGBOX_OK_CANCEL:
+                SDL_EnableUNICODE( wasunicode );
+                SDL_EnableKeyRepeat( wasunicode ? SDL_DEFAULT_REPEAT_DELAY : 0, wasunicode ? SDL_DEFAULT_REPEAT_INTERVAL : 0 );
+                return (presson==0);
+
+              default:
+                SDL_EnableUNICODE( wasunicode );
+                SDL_EnableKeyRepeat( wasunicode ? SDL_DEFAULT_REPEAT_DELAY : 0, wasunicode ? SDL_DEFAULT_REPEAT_INTERVAL : 0 );
+                return SDL_TRUE;
+            }
+            break;
+          }
+
+          presson = -1;
+        }
+        break;
+      
+      case SDL_KEYDOWN:
+        switch( event.key.keysym.sym )
+        {
+          case SDLK_TAB:
+          case SDLK_RIGHT:
+            cbtn = cbtn+1;
+            if( btns[cbtn].x == -1 ) cbtn = 0;
+            msgbox_render( oric );
+            break;
+          
+          case SDLK_LEFT:
+            cbtn--;
+            if( cbtn < 0 )
+            {
+              for( cbtn=0; btns[cbtn].x!=-1; cbtn++ ) ;
+              cbtn--;
+            }
+            msgbox_render( oric );
+            break;
+          
+          case SDLK_RETURN:
+            switch( type )
+            {
+              case MSGBOX_YES_NO:
+              case MSGBOX_OK_CANCEL:
+                SDL_EnableUNICODE( wasunicode );
+                SDL_EnableKeyRepeat( wasunicode ? SDL_DEFAULT_REPEAT_DELAY : 0, wasunicode ? SDL_DEFAULT_REPEAT_INTERVAL : 0 );
+                return (cbtn==0);
+
+              default:
+                SDL_EnableUNICODE( wasunicode );
+                SDL_EnableKeyRepeat( wasunicode ? SDL_DEFAULT_REPEAT_DELAY : 0, wasunicode ? SDL_DEFAULT_REPEAT_INTERVAL : 0 );
+                return SDL_TRUE;
+            }
+            break;
+          
+          default:
+            break;
+        }
+        break;
+    }
+  }
+
+  SDL_EnableUNICODE( wasunicode );
+  SDL_EnableKeyRepeat( wasunicode ? SDL_DEFAULT_REPEAT_DELAY : 0, wasunicode ? SDL_DEFAULT_REPEAT_INTERVAL : 0 );
+  return SDL_FALSE;
+}
