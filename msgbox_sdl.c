46888fcc8e18d276cbc9059347d94987e29bbeb8
revolf <revolf@7be9efca-6d47-11de-8405-1fc01b05f06c>
2010-03-21 17:26:29 +0000

    Fix comments.

--- msgbox_sdl.c
+++ msgbox_sdl.c
@@ -16,7 +16,7 @@
 **  along with this program; if not, write to the Free Software
 **  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **
-**  SDL based file requester
+**  SDL based message box
 */
 
 #include <stdlib.h>
