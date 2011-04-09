f57165ec9d7061df9651f20288ae66a5d119ced7
revolf <revolf@7be9efca-6d47-11de-8405-1fc01b05f06c>
2011-04-09 18:20:18 +0000

    Partial attempt at a native popup menu and about box for BeOS. Disabled for now.

--- trunk/gui_beos.cpp
+++ trunk/gui_beos.cpp
@@ -25,10 +25,14 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <Alert.h>
 #include <Clipboard.h>
 #include <Font.h>
 #include <InterfaceDefs.h>
 #include <List.h>
+#include <MenuItem.h>
+#include <Mime.h>
+#include <PopUpMenu.h>
 #include <String.h>
 #include <TextView.h>
 
@@ -55,9 +59,113 @@ static rgb_color oric_colors[] = {
 	{ 255, 255, 255, 255 }
 };
 
+extern struct osdmenu menus[];
+extern struct osdmenuitem aboutitems[];
+static BPopUpMenu *sPopup;
+
+class OSDMenuItem : public BMenuItem {
+public:
+	OSDMenuItem(struct osdmenuitem *desc, const char *name, BMessage *message)
+		: BMenuItem(name, message), fItem(desc)
+		{
+			if (desc->name[0] == '\x0e')
+				SetMarked(true);
+			if (desc->key)
+				SetTrigger(desc->key[0]);
+		};
+	virtual ~OSDMenuItem() {};
+	struct osdmenuitem *fItem;
+};
+
+static void build_native_menus( struct osdmenu *menu, BMenu *m )
+{
+	struct osdmenuitem *item = menu->items;
+
+	printf("menu '%s': {\n", menu->title);
+	for (; item->name; item++) {
+		BMenuItem *mi;
+		const char *name = item->name;
+		if (name == OSDMENUBAR) {
+			name = "------------";
+			// skip separators before back since it will also be ignored
+			if (item[1].sdlkey == SDLK_BACKSPACE)
+				continue;
+			mi = new BSeparatorItem();
+		} else if (item->func == gotomenu) {
+			if (item->arg == 0)
+				continue;
+			// about item is special
+			if (menus[item->arg].items == aboutitems) {
+				mi = new BMenuItem(name, new BMessage(B_ABOUT_REQUESTED));
+			} else {
+				BMenu *subMenu = new BMenu(name);
+				mi = new BMenuItem(subMenu);
+				build_native_menus( &menus[item->arg], subMenu );
+			}
+		} else {
+			if (name[0] == ' ' || name[0] == '\x0e')
+				name = &name[1];
+			mi = new OSDMenuItem(item, name, NULL);
+		}
+			
+		//printf("item '%s' key '%s'\n", name, item->key);
+
+		m->AddItem(mi);
+	}
+	printf("}\n");
+}
+
+static void run_native_about( void )
+{
+	struct osdmenuitem *item = aboutitems;
+	BString text;
+	for (; item->name; item++) {
+		if (item->name == OSDMENUBAR)
+			break;
+		text << item->name;
+		text << "\n";
+	}
+	BAlert *alert = new BAlert("About", text.String(), "Ok");
+	alert->Go();
+}
+
 SDL_bool init_gui_native( struct machine *oric )
 {
-  return SDL_TRUE;
+	BMimeType tapeType("application/x-oric-tape");
+	if (!tapeType.IsInstalled())
+		tapeType.Install();
+	BMessage exts;
+	if (tapeType.GetFileExtensions(&exts) != B_OK) {
+		exts.AddString("extensions", "tap");
+		tapeType.SetShortDescription("ORIC Tape");
+		tapeType.SetLongDescription("ORIC Tape Image for emulators");
+		tapeType.SetPreferredApp("application/x-vnd.oricutron");
+		tapeType.SetFileExtensions(&exts);
+		tapeType.SetSnifferRule("0.40      [0:8](\"\\026\\026\\026\\044\")");
+	}
+	exts.MakeEmpty();
+	BMimeType diskType("application/x-oric-disk");
+	if (!diskType.IsInstalled())
+		diskType.Install();
+	if (diskType.GetFileExtensions(&exts) != B_OK) {
+		exts.AddString("extensions", "dsk");
+		diskType.Install();
+		diskType.SetShortDescription("ORIC Disk");
+		diskType.SetLongDescription("ORIC Disk Image for emulators");
+		diskType.SetPreferredApp("application/x-vnd.oricutron");
+		diskType.SetFileExtensions(&exts);
+		diskType.SetSnifferRule("0.80      (\"MFM_DISK\")");
+	}
+	//BMimeType romType("application/x-oric-rom");
+
+	//sPopup = new BPopUpMenu("main");
+	//build_native_menus( menus, sPopup );
+	//TEST
+	//sPopup->Go( BPoint(0, 0), false, true);
+
+	//run_native_about();
+
+	return SDL_TRUE;
 }
 
 void shut_gui_native( struct machine *oric )
