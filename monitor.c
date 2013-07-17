f5afa4257ce5359a8a1d3316963f55b5d54064ea
pete <pete@petergordon.org.uk>
2013-07-17 22:34:42 +0000

    Fixed a crash when you start Oricutron in Atmos mode, then change the machine type to Pravetz, then the disk type to Pravetz.

--- monitor.c
+++ monitor.c
@@ -3900,7 +3900,7 @@ static unsigned int steppy_step( struct machine *oric )
   tape_patches( oric );
   via_clock( &oric->via, oric->cpu.icycles );
   ay_ticktock( &oric->ay, oric->cpu.icycles );
-  if( oric->drivetype ) wd17xx_ticktock( &oric->wddisk, oric->cpu.icycles );
+  if((oric->drivetype == DRV_MICRODISC) || (oric->drivetype == DRV_JASMIN)) wd17xx_ticktock( &oric->wddisk, oric->cpu.icycles );
   if( oric->type == MACH_TELESTRAT )
   {
     via_clock( &oric->tele_via, oric->cpu.icycles );
@@ -4006,7 +4006,7 @@ SDL_bool mon_event( SDL_Event *ev, struct machine *oric, SDL_bool *needrender )
           tape_patches( oric );
           via_clock( &oric->via, oric->cpu.icycles );
           ay_ticktock( &oric->ay, oric->cpu.icycles );
-          if( oric->drivetype ) wd17xx_ticktock( &oric->wddisk, oric->cpu.icycles );
+          if((oric->drivetype == DRV_MICRODISC) || (oric->drivetype == DRV_JASMIN)) wd17xx_ticktock( &oric->wddisk, oric->cpu.icycles );
           if( oric->type == MACH_TELESTRAT )
           {
             via_clock( &oric->tele_via, oric->cpu.icycles );
