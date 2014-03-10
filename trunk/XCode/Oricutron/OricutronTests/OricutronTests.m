4124039f1b5c2782825ac96d36b340bf56167c26
torguet <torguet@7be9efca-6d47-11de-8405-1fc01b05f06c>
2014-03-10 21:08:00 +0000

    Added Xcode project files

---
+++ trunk/XCode/Oricutron/OricutronTests/OricutronTests.m
@@ -0,0 +1,34 @@
+//
+//  OricutronTests.m
+//  OricutronTests
+//
+//  Created by Patrice Torguet on 07/03/2014.
+//  Copyright (c) 2014 Peter Gordon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OricutronTests : XCTestCase
+
+@end
+
+@implementation OricutronTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
