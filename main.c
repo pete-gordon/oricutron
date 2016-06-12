9b5a2d8c54989a40cf6fae78c4b42b69d8c74def
Andreas Schniertshauer <andreas@schniertshauer.photography>
2016-06-12 15:02:02 +0200

    Add: Enabling Memory Leak Detection for Visual Studio builds see https://msdn.microsoft.com/library/x98tx3cf.aspx

--- main.c
+++ main.c
@@ -22,6 +22,11 @@
 #include "CoreFoundation/CoreFoundation.h"
 #endif
 
+#ifdef _MSC_VER
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -1299,6 +1304,10 @@ void once_per_frame( struct machine *oric )
 
 int main( int argc, char *argv[] )
 {
+#ifdef _MSC_VER
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
   static struct machine oric;
   SDL_bool isinit;
 
