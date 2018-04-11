pete-gordon/oricutron

commit 24b3a4bf43f261543fd262e67e635643584d09b8

- load breakpoints from command line - adding flags to CPU breakpoints - correct timings related to vsync hack

snapshot.c
@@ -171,14 +171,14 @@ SDL_bool save_snapshot(struct machine *oric, char *filename)
   PUTU8(oric->type);            //  0
   PUTU32(oric->overclockmult);  //  1
   PUTU32(oric->overclockshift); //  5
-  PUTU8(oric->vsync);           //  9
-  PUTU8(oric->romdis);          // 10
-  PUTU8(oric->romon);           // 11
-  PUTU8(oric->vsynchack);       // 12
-  PUTU8(oric->drivetype);       // 13
-  PUTU8(oric->tapeturbo);       // 14
-  PUTU8(oric->vid_mode);        // 15
-  PUTU32(oric->keymap);         // 16 = 20
+  PUTU16(oric->vsync);          //  9
+  PUTU8(oric->romdis);          // 11
+  PUTU8(oric->romon);           // 12
+  PUTU8(oric->vsynchack);       // 13
+  PUTU8(oric->drivetype);       // 14
+  PUTU8(oric->tapeturbo);       // 15
+  PUTU8(oric->vid_mode);        // 16
+  PUTU32(oric->keymap);         // 17 = 21
   DATABLOCK(oric->mem, oric->memsize);
 
   NEWBLOCK("TAP\x00");
@@ -855,7 +855,7 @@ SDL_bool load_snapshot(struct machine *oric, char *filename)
   blk->offs = 1;
   oric->overclockmult  = getu32(blk);
   oric->overclockshift = getu32(blk);
-  oric->vsync          = getu8 (blk);
+  oric->vsync          = getu16(blk);
   oric->romdis         = getu8 (blk);
   oric->romon          = getu8 (blk);
   oric->vsynchack      = getu8 (blk);

6502.c
@@ -313,7 +313,10 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
       for( i=0; i<16; i++ )
       {
         if( ( cpu->breakpoints[i] != -1 ) && ( cpu->calcpc == cpu->breakpoints[i] ) )
-          return SDL_TRUE;
+        {
+          if( (cpu->breakpoint_flags[i] & MBPF_RESETCYCLES) ==  MBPF_RESETCYCLES ) cpu->cycles = 0;
+          if( (cpu->breakpoint_flags[i] & MBPF_RESETCYCLESCONTINUE) !=  MBPF_RESETCYCLESCONTINUE ) return SDL_TRUE;
+        }
       }
     }
 
@@ -323,7 +326,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
 
       waddr = raddr = 0;
       wlen = rlen = 0;
-    
+
       // Going to read or write?
       switch( cpu->calcop )
       {
@@ -426,7 +429,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
 #endif
 		  RW_BADDR_ZPX;
           break;
-        
+
 #ifdef ILLEGALS
         case 0xDB: // { "DCP", AM_ABY },  // DB (illegal)
         case 0xFB: // { "ISC", AM_ABY },  // FB (illegal)
@@ -436,7 +439,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
         case 0x5B: // { "SRE", AM_ABY },  // 5B (illegal)
           RW_BADDR_ABY;
           break;
-        
+
         case 0xC3: // { "DCP", AM_ZIX },  // C3 (illegal)
         case 0xE3: // { "ISC", AM_ZIX },  // E3 (illegal)
         case 0x23: // { "RLA", AM_ZIX },  // 23 (illegal)
@@ -445,7 +448,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
         case 0x43: // { "SRE", AM_ZIX },  // 43 (illegal)
           RW_BADDR_ZIX;
           break;
-        
+
         case 0xD3: // { "DCP", AM_ZIY },  // D3 (illegal)
         case 0xF3: // { "ISC", AM_ZIY },  // F3 (illegal)
         case 0x33: // { "RLA", AM_ZIY },  // 33 (illegal)
@@ -542,7 +545,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
         case 0xBF: // { "LAX", AM_ABY },  // BF (illegal)
 #endif
 		  R_BADDR_ABY;
-          break;    
+          break;
 
         case 0x1D: // { "ORA", AM_ABX },  // 1D
         case 0x3D: // { "AND", AM_ABX },  // 3D
@@ -553,7 +556,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
         case 0xDD: // { "CMP", AM_ABX },  // DD
         case 0xFD: // { "SBC", AM_ABX },  // FD
           R_BADDR_ABX;
-          break;    
+          break;
 
         case 0xB6: // { "LDX", AM_ZPY },  // B6
 #ifdef ILLEGALS
@@ -566,7 +569,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
           raddr = (cpu->read( cpu, cpu->calcpc+2 )<<8)|cpu->read( cpu, cpu->calcpc+1 );
           rlen = 2;
           break;
-          
+
         case 0x81: // { "STA", AM_ZIX },  // 81
 #ifdef ILLEGALS
         case 0x83: // { "SAX", AM_ZIX },  // 83 (illegal)
@@ -582,7 +585,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
 #endif
 		  W_BADDR_ZP;
           break;
-         
+
         case 0x8C: // { "STY", AM_ABS },  // 8C
         case 0x8D: // { "STA", AM_ABS },  // 8D
         case 0x8E: // { "STX", AM_ABS },  // 8E
@@ -917,7 +920,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
       cpu->icycles = 4;
       cpu->baddr = baddr+cpu->y;
       if( CPAGECHECK ) cpu->icycles++;
-      break;    
+      break;
 
     case 0x1D: // { "ORA", AM_ABX },  // 1D
     case 0x3D: // { "AND", AM_ABX },  // 3D
@@ -937,7 +940,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
       cpu->icycles = 4;
       cpu->baddr = baddr+cpu->x;
       if( CPAGECHECK ) cpu->icycles++;
-      break;    
+      break;
 
     case 0x30: // { "BMI", AM_REL },  // 30
       IBRANCH( cpu->f_n );
@@ -950,7 +953,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
     case 0x70: // { "BVS", AM_REL },  // 70
       IBRANCH( cpu->f_v );
       break;
-    
+
     case 0x90: // { "BCC", AM_REL },  // 90
       IBRANCH( !cpu->f_c );
       break;
@@ -971,7 +974,7 @@ SDL_bool m6502_set_icycles( struct m6502 *cpu, SDL_bool dobp, char *bpmsg )
       cpu->icycles = 6;
       break;
   }
-  
+
   cpu->icycles += extra;
   return SDL_FALSE;
 }
@@ -1074,7 +1077,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       DO_ASL(v);
       cpu->write( cpu, baddr, v );
       break;
-    
+
     case 0x18: // { "CLC", AM_IMP },  // 18
       cpu->f_c = 0;
       break;
@@ -1084,7 +1087,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       v = cpu->read( cpu, cpu->baddr );  // baddr is already calculated for this case
       cpu->pc+=2;
       DO_ORA;
-      break;    
+      break;
 
     case 0x1E: // { "ASL", AM_ABX },  // 1E
       READ_ABX;
@@ -1102,7 +1105,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       READ_ZIX;
       DO_AND;
       break;
-    
+
     case 0x24: // { "BIT", AM_ZP  },  // 24
       READ_ZP;
       cpu->f_n = v&0x80 ? 1 : 0;
@@ -1130,7 +1133,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       READ_IMM;
       DO_AND;
       break;
-    
+
     case 0x2A: // { "ROL", AM_IMP },  // 2A
       DO_ROL(cpu->a);
       break;
@@ -1146,7 +1149,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       READ_ABS;
       DO_AND;
       break;
-    
+
     case 0x2E: // { "ROL", AM_ABS },  // 2E
       KREAD_ABS;
       DO_ROL(v);
@@ -1183,7 +1186,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       v = cpu->read( cpu, cpu->baddr );  // baddr is already calculated for this case
       cpu->pc+=2;
       DO_AND;
-      break;    
+      break;
 
     case 0x3E: // { "ROL", AM_ABX },  // 3E
       READ_ABX;
@@ -1271,7 +1274,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       v = cpu->read( cpu, cpu->baddr );  // baddr is already calculated for this case
       cpu->pc+=2;
       DO_EOR;
-      break;    
+      break;
 
     case 0x5E: // { "LSR", AM_ABX },  // 5E
       READ_ABX;
@@ -1309,7 +1312,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       READ_IMM;
       DO_ADC;
       break;
-    
+
     case 0x6A: // { "ROR", AM_IMP },  // 6A
       DO_ROR(cpu->a);
       break;
@@ -1318,12 +1321,12 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       baddr = (cpu->read( cpu, cpu->pc+1 )<<8)|cpu->read( cpu, cpu->pc );
       cpu->pc = (cpu->read( cpu, baddr+1 )<<8)|cpu->read( cpu, baddr );
       break;
-      
+
     case 0x6D: // { "ADC", AM_ABS },  // 6D
       READ_ABS;
       DO_ADC;
       break;
-    
+
     case 0x6E: // { "ROR", AM_ABS },  // 6E
       KREAD_ABS;
       DO_ROR(v);
@@ -1333,18 +1336,18 @@ SDL_bool m6502_inst( struct m6502 *cpu )
     case 0x70: // { "BVS", AM_REL },  // 70
       BRANCH( cpu->f_v );
       break;
-    
+
     case 0x71: // { "ADC", AM_ZIY },  // 71
       v = cpu->read( cpu, cpu->baddr );  // baddr is already calculated for this case
       cpu->pc++;
       DO_ADC;
       break;
-    
+
     case 0x75: // { "ADC", AM_ZPX },  // 75
       READ_ZPX;
       DO_ADC;
       break;
-    
+
     case 0x76: // { "ROR", AM_ZPX },  // 76
       KREAD_ZPX;
       DO_ROR(v);
@@ -1360,7 +1363,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       v = cpu->read( cpu, cpu->baddr );  // baddr is already calculated for this case
       cpu->pc+=2;
       DO_ADC;
-      break;    
+      break;
 
     case 0x7E: // { "ROR", AM_ABX },  // 7E
       READ_ABX;
@@ -1460,7 +1463,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       cpu->x = v;
       FLAG_ZN(cpu->x);
       break;
-      
+
     case 0xA4: // { "LDY", AM_ZP  },  // A4
       READ_ZP;
       cpu->y = v;
@@ -1574,7 +1577,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       r = cpu->y-v;
       FLAG_SZCN(r);
       break;
-      
+
     case 0xC1: // { "CMP", AM_ZIX },  // C1
       READ_ZIX;
       r = cpu->a-v;
@@ -1598,7 +1601,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       cpu->write( cpu, baddr, --v );
       FLAG_ZN(v);
       break;
-     
+
     case 0xC8: // { "INY", AM_IMP },  // C8
       cpu->y++;
       FLAG_ZN(cpu->y);
@@ -1632,7 +1635,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       cpu->write( cpu, baddr, --v );
       FLAG_ZN(v);
       break;
-      
+
     case 0xD0: // { "BNE", AM_REL },  // D0
       BRANCH( !cpu->f_z );
       break;
@@ -1648,7 +1651,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       r = cpu->a-v;
       FLAG_SZCN(r);
       break;
-    
+
     case 0xD6: // { "DEC", AM_ZPX },  // D6
       KREAD_ZPX;
       cpu->write( cpu, baddr, --v );
@@ -1763,7 +1766,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       v = cpu->read( cpu, cpu->baddr );  // baddr is already calculated for this case
       cpu->pc+=2;
       DO_SBC;
-      break;    
+      break;
 
     case 0xFE: // { "INC", AM_ABX },  // FE
       READ_ABX;
@@ -1779,7 +1782,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       DO_AND;
       cpu->f_c = cpu->f_n;
       break;
-    
+
     case 0x87: // { "SAX", AM_ZP  },  // 87 (illegal)
       v = cpu->a & cpu->x;
       WRITE_ZP(v);
@@ -1841,9 +1844,9 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       // Instability 1 (sometimes, the &H drops off)
       if ((cpu->cycles&7)>2) // pseudorandom 5 in 8 chance of &H
         r &= (cpu->pc>>8)+1;
-      
+
       v = cpu->a & r;
-      
+
       // Instability 2 (A is input and output, which leads to "bit fight",
       // but anywhere that X&H=1 will work)
       if (((cpu->cycles>>8)&7)<2) // pseudorandom 2 in 8 chance of bit fight
@@ -2408,7 +2411,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
 
     case 0x8B: // { "XAA", AM_IMM },  // 8B (illegal, unstable)
       READ_IMM;
-      
+
       cpu->a = cpu->x & v;
 
       // Instability
@@ -2433,7 +2436,7 @@ SDL_bool m6502_inst( struct m6502 *cpu )
       return SDL_TRUE; // jammed
 #endif
   }
-  
+
   return SDL_FALSE;
 }
 

6502.h
@@ -54,6 +54,10 @@
 #define MBPF_WRITE (1<<MBPB_WRITE)
 #define MBPB_CHANGE 2
 #define MBPF_CHANGE (1<<MBPB_CHANGE)
+#define MBPB_RESETCYCLES 3
+#define MBPF_RESETCYCLES (1<<MBPB_RESETCYCLES)
+#define MBPB_RESETCYCLESCONTINUE 4
+#define MBPF_RESETCYCLESCONTINUE (1<<MBPB_RESETCYCLESCONTINUE)
 
 // Merge the seperate flag stores into a 6502 status register form
 #define MAKEFLAGS ((cpu->f_n<<7)|(cpu->f_v<<6)|(1<<5)|(cpu->f_b<<4)|(cpu->f_d<<3)|(cpu->f_i<<2)|(cpu->f_z<<1)|cpu->f_c)
@@ -86,6 +90,7 @@ struct m6502
   unsigned char (*read)(struct m6502 *,Uint16);
   SDL_bool anybp, anymbp;
   Sint32   breakpoints[16];
+  Uint8    breakpoint_flags[16];
   struct membreakpoint membreakpoints[16];
   void    *userdata;
 

6551_modem.c
@@ -23,7 +23,7 @@
 // to cope with getaddrinfo's bug: http://programmingrants.blogspot.fr/2009/09/tips-on-undefined-reference-to.html
 #define _WIN32_WINNT 0x0501
 #endif
- 
+
 #ifdef _MSC_VER
 // Need to link with Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
@@ -158,11 +158,11 @@ static void mdm_connect(const char* s)
   char ip[1024];
   char* p = NULL;
   int port = 0;
-
+  
   strcpy(ip, s);
   trim(ip);
   p = strrchr(ip, ':');
-
+  
   if(p)
   {
     port = atoi(p+1);
@@ -170,7 +170,7 @@ static void mdm_connect(const char* s)
   }
   // default telnet port
   if(port == 0) port = ACIA_TYPE_MODEM_DEFAULT_PORT;
-
+  
   if( socket_create(&cnt_sck, oric->aciabackendcfgdomain) )
   {
     if(socket_connect(cnt_sck, ip, port, oric->aciabackendcfgdomain))
@@ -184,7 +184,7 @@ static void mdm_connect(const char* s)
   }
   else
     send_responce("NO DIALTINE");
-
+  
   mdm_hangup();
 }
 
@@ -192,7 +192,7 @@ static void parse_command(const char* s)
 {
   if(!s)
     return;
-
+  
   if( escaped )
   {
     if('\0' == s[0])
@@ -291,10 +291,10 @@ static void parse_command(const char* s)
 static void modem_done( struct acia* acia )
 {
   mdm_hangup();
-
+  
   socket_close(srv_sck);
   srv_sck = -1;
-
+  
   socket_done();
   // acia_init_none( acia );
 }
@@ -315,7 +315,7 @@ static Uint8 modem_stat(Uint8 stat)
         mdm_out += len;
     }
   }
-
+  
   if( !connected && listening )
   {
     if(socket_accept(srv_sck, &cnt_sck))
@@ -324,13 +324,13 @@ static Uint8 modem_stat(Uint8 stat)
       connected = SDL_TRUE;
     }
   }
-
+  
   if( connected )
     return (stat & ~(ASTF_CARRIER|ASTF_DSR));
-
+  
   if( listening )
     return (stat & ~(ASTF_DSR));
-
+  
   return (stat | (ASTF_CARRIER|ASTF_DSR));
 }
 
@@ -341,7 +341,7 @@ static SDL_bool modem_has_byte(Uint8* data)
     *data = mdm_out_buf[0];
     return SDL_TRUE;
   }
-
+  
   return SDL_FALSE;
 }
 
@@ -354,7 +354,7 @@ static SDL_bool modem_get_byte(Uint8* data)
     mdm_out--;
     return SDL_TRUE;
   }
-
+  
   return SDL_FALSE;
 }
 
@@ -436,10 +436,10 @@ static SDL_bool modem_put_byte(Uint8 data)
 SDL_bool acia_init_modem( struct acia* acia )
 {
   oric = acia->oric;
-
+  
   mdm_in = 0;
   mdm_out = 0;
-
+  
   mdm_in_buf[0] = 0x00;
   mdm_out_buf[0] = 0x00;
   memset(mdm_cmd_buf, 0, sizeof(Uint8)*CMD_BUF_SIZE);
@@ -450,10 +450,10 @@ SDL_bool acia_init_modem( struct acia* acia )
   acia->has_byte = modem_has_byte;
   acia->get_byte = modem_get_byte;
   acia->put_byte = modem_put_byte;
-
+  
   srv_sck = -1;
   cnt_sck = -1;
-
+  
   connected = SDL_FALSE;
   listening = SDL_FALSE;
   escaped = SDL_FALSE;
@@ -463,7 +463,7 @@ SDL_bool acia_init_modem( struct acia* acia )
     oric->aciabackend = ACIA_TYPE_MODEM;
     return SDL_TRUE;
   }
-
+  
   // fall-back to none
   acia_init_none( acia );
   return SDL_FALSE;
@@ -488,11 +488,11 @@ struct Library *SocketBase;
 typedef unsigned long socklen_t;
 char *inet_ntoa(struct in_addr n)
 {
-static char a[sizeof "XXX.XXX.XXX.XXX"];
-char *p = (char *)&n;
-
-snprintf(a, sizeof a, "%d.%d.%d.%d", p[0]&0xff, p[1]&0xff, p[2]&0xff, p[3]&0xff);
-return a;
+  static char a[sizeof "XXX.XXX.XXX.XXX"];
+  char *p = (char *)&n;
+  
+  snprintf(a, sizeof a, "%d.%d.%d.%d", p[0]&0xff, p[1]&0xff, p[2]&0xff, p[3]&0xff);
+  return a;
 }
 #endif
 #endif
@@ -525,16 +525,16 @@ static SDL_bool socket_init(void)
 {
   if(!socket_initialized)
   {
-    #ifdef WIN32
+#ifdef WIN32
     WSADATA wsadata;
     if( (WSAStartup(MAKEWORD(2,2), &wsadata) == SOCKET_ERROR) ||
       (WSAStartup(MAKEWORD(1,1), &wsadata) == SOCKET_ERROR) )
       return SDL_FALSE;
-    #endif
-    #ifdef __MORPHOS__
+#endif
+#ifdef __MORPHOS__
     if( !(SocketBase = OldOpenLibrary("bsdsocket.library")) )
       return SDL_FALSE;
-    #endif
+#endif
   }
   socket_initialized = SDL_TRUE;
   return SDL_TRUE;
@@ -542,13 +542,13 @@ static SDL_bool socket_init(void)
 
 static void socket_done(void)
 {
-  #ifdef WIN32
+#ifdef WIN32
   WSACleanup();
-  #endif
-  #ifdef __MORPHOS__
+#endif
+#ifdef __MORPHOS__
   if( SocketBase )
     CloseLibrary(SocketBase);
-  #endif
+#endif
 }
 
 static int socket_create(int* sock, int domain)
@@ -558,21 +558,21 @@ static int socket_create(int* sock, int domain)
   domain = AF_INET;
 #else
   if (domain == 6)
-      domain = AF_INET6;
+    domain = AF_INET6;
   else
-      domain = AF_INET;
+    domain = AF_INET;
 #endif
   *sock = socket(domain, SOCK_STREAM, 0);
   if(*sock == -1)
     return 0;
-
+  
   if(setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, (const char*) &on, sizeof(on)) == -1)
     return 0;
 #ifdef __APPLE__
   if(setsockopt(*sock, SOL_SOCKET, SO_REUSEPORT, (const char*) &on, sizeof(on)) == -1)
     return 0;
 #endif
-
+  
   return 1;
 }
 
@@ -581,62 +581,62 @@ static int socket_create(int* sock, int domain)
 
 static int socket_bind(int sock, int port, int domain)
 {
-
+  
 #ifdef NO_GETADDRINFO
   struct sockaddr_in srv_addr;
   memset((void*)&srv_addr, 0, sizeof(srv_addr));
   srv_addr.sin_family = AF_INET;
   srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   srv_addr.sin_port = htons(port);
-
-    if(bind(sock, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) == -1) {
-        perror("socket_bind, bind error: ");
-        return 0;
-    }
+  
+  if(bind(sock, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) == -1) {
+    perror("socket_bind, bind error: ");
+    return 0;
+  }
 #else
-    struct addrinfo	hints, *res, *ressave;
-    char service[NI_MAXSERV];
-    int n;
-    
-    memset(&hints, 0, sizeof(hints));
-	hints.ai_flags = AI_PASSIVE;
-    if (domain == 6)
-        hints.ai_family = AF_INET6;
-    else
-        hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-    
-    sprintf(service, "%d", port);
-    
-	if ( (n = getaddrinfo(NULL, service, &hints, &res)) != 0)  {
-        printf("socket_bind, getaddrinfo error: %s", gai_strerror(n));
-        return 0;
-	}
-	ressave = res;
-    
-    // loop through all the addresses that we got
-	do {
-		if (bind(sock, res->ai_addr, res->ai_addrlen) == 0)
-			break;			/* success */
-	} while ( (res = res->ai_next) != NULL);
-	
-	freeaddrinfo(ressave);
-    
-	if (res == NULL) {
-        perror("socket_bind, bind error: ");
-        return 0;
-	}
+  struct addrinfo	hints, *res, *ressave;
+  char service[NI_MAXSERV];
+  int n;
+  
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_flags = AI_PASSIVE;
+  if (domain == 6)
+    hints.ai_family = AF_INET6;
+  else
+    hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  
+  sprintf(service, "%d", port);
+  
+  if ( (n = getaddrinfo(NULL, service, &hints, &res)) != 0)  {
+    printf("socket_bind, getaddrinfo error: %s", gai_strerror(n));
+    return 0;
+  }
+  ressave = res;
+  
+  // loop through all the addresses that we got
+  do {
+    if (bind(sock, res->ai_addr, res->ai_addrlen) == 0)
+      break;			/* success */
+  } while ( (res = res->ai_next) != NULL);
+  
+  freeaddrinfo(ressave);
+  
+  if (res == NULL) {
+    perror("socket_bind, bind error: ");
+    return 0;
+  }
 #endif
-    
-    
-  #if defined(__LINUX__) || defined(__APPLE__) || defined(__amigaos4__) || defined(__MORPHOS__)
-    fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
-  #endif
-
-  #ifdef WIN32
-    u_long imode = 1;
-    ioctlsocket(sock, FIONBIO, &imode);
-  #endif
+  
+  
+#if defined(__LINUX__) || defined(__APPLE__) || defined(__amigaos4__) || defined(__MORPHOS__)
+  fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
+#endif
+  
+#ifdef WIN32
+  u_long imode = 1;
+  ioctlsocket(sock, FIONBIO, &imode);
+#endif
   return 1;
 }
 
@@ -644,7 +644,7 @@ static int socket_listen(int sock)
 {
   if(listen(sock, 1) == -1)
     return 0;
-
+  
   return 1;
 }
 
@@ -656,22 +656,22 @@ static int socket_accept(int sock, int* sck)
   struct sockaddr_storage cli_addr;
 #endif
   socklen_t cli_addr_len = sizeof(cli_addr);
-
+  
   *sck = accept(sock, (struct sockaddr*) &cli_addr, &cli_addr_len);
   if(*sck == -1)
     return 0;
   else
   {
-    #if defined(__LINUX__) || defined(__amigaos4__) || defined(__MORPHOS__)
+#if defined(__LINUX__) || defined(__amigaos4__) || defined(__MORPHOS__)
     fcntl(*sck, F_SETFL, fcntl(*sck, F_GETFL, 0) | O_NONBLOCK);
-    #endif
-
+#endif
+    
 #if defined(WIN32)
     u_long imode = 1;
     ioctlsocket(*sck, FIONBIO, &imode);
 #endif
   }
-
+  
   return 1;
 }
 
@@ -717,57 +717,57 @@ static int socket_connect(int sock, const char* host, int port, int domain)
   struct sockaddr_in addr;
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
-
+  
   hostname_to_ip(host, ip, 64);
   addr.sin_addr.s_addr = inet_addr(ip);
-
+  
   if(connect(sock, (struct sockaddr*) &addr, sizeof(addr)) != 0) {
     perror("socket_connect, connect error: ");
     return 0;
   }
 #else
-    struct addrinfo	hints, *res, *ressave;
-    char service[NI_MAXSERV];
-    int n;
-    
-    memset(&hints, 0, sizeof(hints));
-    if (domain == 6)
-        hints.ai_family = AF_INET6;
-    else
-        hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-    
-    sprintf(service, "%d", port);
-    
-	if ( (n = getaddrinfo(host, service, &hints, &res)) != 0)  {
-        printf("socket_connect, getaddrinfo error: %s", gai_strerror(n));
-        return 0;
-	}
-	ressave = res;
-    
-    // loop through all the addresses that we got
-	do {
-		if (connect(sock, res->ai_addr, res->ai_addrlen) == 0)
-			break;			/* success */
-	} while ( (res = res->ai_next) != NULL);
- 
-	freeaddrinfo(ressave);
- 
-	if (res == NULL) {
-        perror("socket_bind, connect error: ");
-        return 0;
-	}
+  struct addrinfo	hints, *res, *ressave;
+  char service[NI_MAXSERV];
+  int n;
+  
+  memset(&hints, 0, sizeof(hints));
+  if (domain == 6)
+    hints.ai_family = AF_INET6;
+  else
+    hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  
+  sprintf(service, "%d", port);
+  
+  if ( (n = getaddrinfo(host, service, &hints, &res)) != 0)  {
+    printf("socket_connect, getaddrinfo error: %s", gai_strerror(n));
+    return 0;
+  }
+  ressave = res;
+  
+  // loop through all the addresses that we got
+  do {
+    if (connect(sock, res->ai_addr, res->ai_addrlen) == 0)
+      break;			/* success */
+  } while ( (res = res->ai_next) != NULL);
+  
+  freeaddrinfo(ressave);
+  
+  if (res == NULL) {
+    perror("socket_bind, connect error: ");
+    return 0;
+  }
 #endif
   
-  #if defined(__LINUX__) || defined(__APPLE__) || defined(__amigaos4__) || defined(__MORPHOS__)
-    fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
-  #endif
-
-  #ifdef WIN32
-    u_long imode = 1;
-    ioctlsocket(sock, FIONBIO, &imode);
-  #endif
-
+#if defined(__LINUX__) || defined(__APPLE__) || defined(__amigaos4__) || defined(__MORPHOS__)
+  fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
+#endif
+  
+#ifdef WIN32
+  u_long imode = 1;
+  ioctlsocket(sock, FIONBIO, &imode);
+#endif
+  
   return 1;
 }
 
@@ -775,7 +775,7 @@ int socket_close(int sock)
 {
   if(_closesocket(sock) == -1)
     return 0;
-
+  
   return 1;
 }
 

main.c
@@ -380,7 +380,7 @@ static void load_config( struct start_opts *sto, struct machine *oric )
   char tbtmp[32];
   char keymap_file[4096];
 
-  f = fopen( FILEPREFIX"oricutron.cfg", "r" );
+            f = fopen( FILEPREFIX"oricutron.cfg", "r" );
   if( !f ) return;
 
   while( !feof( f ) )
@@ -1125,17 +1125,46 @@ SDL_bool init( struct machine *oric, int argc, char *argv[] )
 
   if( sto->start_breakpoint )
   {
-    int i = 0;
-    unsigned int addr;
-    if( !mon_getnum( oric, &addr, sto->start_breakpoint, &i, SDL_FALSE, SDL_FALSE, SDL_FALSE, SDL_TRUE ) )
+    if( sto->start_breakpoint[0] == ':' )
     {
-      error_printf( "Invalid breakpoint" );
-      free( sto );
-      return SDL_FALSE;
+      SDL_bool needrender = SDL_FALSE;;
+      char bsbuf[32];
+      char* fn = sto->start_breakpoint + 1;
+      FILE* f = fopen( fn, "rt" );
+      if( !f )
+      {
+        error_printf( "Invalid breakpoint file" );
+        free( sto );
+        return SDL_FALSE;
+      }        
+      while( !feof( f ) )
+      {
+        char* p;
+        if( !fgets( bsbuf, 32, f ) ) break;
+        bsbuf[31] = 0;
+        while( NULL != (p = strchr( bsbuf, '\r' )) ) *p = 0;
+        while( NULL != (p = strchr( bsbuf, '\n' )) ) *p = 0;
+        while( NULL != (p = strchr( bsbuf, '\t' )) ) *p = ' ';
+        p = bsbuf;
+        while( *p && isws( *p ) ) p++;
+        if( *p ) mon_cmd( bsbuf, oric, &needrender );
+      }
+      fclose( f );
     }
-
-    oric->cpu.breakpoints[0] = addr;
-    oric->cpu.anybp = SDL_TRUE;
+    else
+    {
+      int i = 0;
+      unsigned int addr;
+      if( !mon_getnum( oric, &addr, sto->start_breakpoint, &i, SDL_FALSE, SDL_FALSE, SDL_FALSE, SDL_TRUE ) )
+      {
+        error_printf( "Invalid breakpoint" );
+        free( sto );
+        return SDL_FALSE;
+      }
+      
+      oric->cpu.breakpoints[0] = addr;
+      oric->cpu.anybp = SDL_TRUE;
+    }
   }
 
   if( sto->start_snapshot[0] )

monitor.c
@@ -934,7 +934,7 @@ struct msym *mon_tab_find_sym_by_name( struct symboltable *stab, struct machine 
     }
   }
 
-  if( symnum) (*symnum) = -1;
+  if( symnum ) (*symnum) = -1;
   return NULL;
 }
 
@@ -2746,7 +2746,11 @@ SDL_bool mon_cmd( char *cmd, struct machine *oric, SDL_bool *needrender )
           {
             if( oric->cpu.breakpoints[j] != -1 )
             {
-              mon_printf( "%02d: $%04X", j, oric->cpu.breakpoints[j] );
+              mon_printf( "%02d: $%04X %s%s",
+                j, 
+                oric->cpu.breakpoints[j],
+                (oric->cpu.breakpoint_flags[j]&MBPF_RESETCYCLES) ? "z" : "",
+                (oric->cpu.breakpoint_flags[j]&MBPF_RESETCYCLESCONTINUE) ? "c" : "");
               oric->cpu.anybp = SDL_TRUE;
             }
           }
@@ -2833,8 +2837,32 @@ SDL_bool mon_cmd( char *cmd, struct machine *oric, SDL_bool *needrender )
           }
 
           oric->cpu.breakpoints[j] = v & 0xffff;
+          oric->cpu.breakpoint_flags[j] = 0;
           oric->cpu.anybp = SDL_TRUE;
-          mon_printf( "%02d: $%04X", j, oric->cpu.breakpoints[j] );
+
+          while( isws( cmd[i] ) ) i++;
+          
+          for( ;; )
+          {
+            switch( cmd[i] )
+            {
+              case 'z':
+                oric->cpu.breakpoint_flags[j] |= MBPF_RESETCYCLES;
+                i++;
+                continue;
+              case 'c':
+                oric->cpu.breakpoint_flags[j] |= MBPF_RESETCYCLESCONTINUE;
+                i++;
+                continue;
+            }
+            break;
+          }
+
+          mon_printf( "%02d: $%04X %s%s", 
+            j, 
+            oric->cpu.breakpoints[j],
+            (oric->cpu.breakpoint_flags[j]&MBPF_RESETCYCLES) ? "z" : "",
+            (oric->cpu.breakpoint_flags[j]&MBPF_RESETCYCLESCONTINUE) ? "c" : "" );
           break;
 
         case 'c':
@@ -3096,7 +3124,7 @@ SDL_bool mon_cmd( char *cmd, struct machine *oric, SDL_bool *needrender )
 
       }
       *needrender = SDL_TRUE;
-	  updatepreview = SDL_TRUE;
+      updatepreview = SDL_TRUE;
       break;
 
     case 0:
@@ -3237,7 +3265,7 @@ SDL_bool mon_cmd( char *cmd, struct machine *oric, SDL_bool *needrender )
           mon_str( "  bcm <bp id>           - Clear mem breakpoint" );
           mon_str( "  bl                    - List breakpoints" );
           mon_str( "  blm                   - List mem breakpoints" );
-          mon_str( "  bs <addr>             - Set breakpoint" );
+          mon_str( "  bs <addr> [zc]        - Set breakpoint/cycles" );
           mon_str( "  bsm <addr> [rwc]      - Set mem breakpoint" );
           mon_str( "  bz                    - Zap breakpoints" );
           mon_str( "  bzm                   - Zap mem breakpoints" );

monitor.h
@@ -87,6 +87,7 @@ SDL_bool mon_symsfromsnapshot( struct symboltable *stab, unsigned char *buffer, 
 void mon_state_reset( struct machine *oric );
 SDL_bool mon_getnum( struct machine *oric, unsigned int *num, char *buf, int *off, SDL_bool addrregs, SDL_bool nregs, SDL_bool viaregs, SDL_bool symbols );
 SDL_bool mon_do_cmd( char *cmd, struct machine *oric, SDL_bool *needrender );
+SDL_bool mon_cmd( char *cmd, struct machine *oric, SDL_bool *needrender );
 
 #if defined(DEBUG_CPU_TRACE) && DEBUG_CPU_TRACE > 0
 struct cpu_trace_entry

commit 9d2ffcf115b6bb347e5bb41638cda0585fcf8dc0

Fix bug introduced in commit https://github.com/pete-gordon/oricutron/commit/24b3a4bf43f261543fd262e67e635643584d09b8 which prevent Oricutron from loading snapshot file

snapshot.c
@@ -801,7 +801,7 @@ SDL_bool load_snapshot(struct machine *oric, char *filename)
   }
 
   /* Get the main block */
-  blk = load_block(oric, "OSN\x00", f, SDL_TRUE, 20, SDL_TRUE);
+  blk = load_block(oric, "OSN\x00", f, SDL_TRUE, 21, SDL_TRUE);
   if (!blk)
   {
     free_blockheaders();