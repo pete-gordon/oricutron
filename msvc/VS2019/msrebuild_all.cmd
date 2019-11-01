c8a7a22d6a26cc3ad8222f4681967a1b29911ecf
lady mesh <Blonder@users.noreply.github.com>
2019-11-01 18:50:11 +0100

    Add: folder VS2019 fro compiling with Microsoft Visual Studio 2019 Preview using vcpkg needed for the SDL library.

---
+++ msvc/VS2019/msrebuild_all.cmd
@@ -0,0 +1,6 @@
+@echo off
+rem call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvars64.bat"
+
+set MSBUILDTOOL=msbuild.exe
+"%MSBUILDTOOL%" Oricutron.vcxproj /t:Rebuild /p:Configuration=Debug;Platform=x64
+"%MSBUILDTOOL%" Oricutron.vcxproj /t:Rebuild /p:Configuration=Release;Platform=x64
