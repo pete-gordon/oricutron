e233580e858cd327412c7bb72025afed04a397c3
lady mesh <Blonder@users.noreply.github.com>
2019-11-01 23:00:30 +0100

    Get rid of warning strncasecmp already defined.

--- msvc/strcasecmp.h
+++ msvc/strcasecmp.h
@@ -1,9 +1,18 @@
+#ifndef ORICUTRON_STRCASECMP_H
+#define ORICUTRON_STRCASECMP_H
+
 #ifdef _MSC_VER
 #ifdef __SPECIFY_SDL_DIR__
 #include <SDL2/SDL.h>
 #else
 #include <SDL.h>
 #endif
+#ifndef strcasecmp
 #define strcasecmp SDL_strcasecmp
+#endif
+#ifndef strncasecmp
 #define strncasecmp SDL_strncasecmp
 #endif
+#endif
+
+#endif /* ORICUTRON_STRCASECMP_H */
