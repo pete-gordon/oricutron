9f1d351462433307ed1478289c8b9624b580b064
jede <jede@oric.org>
2020-04-22 00:40:39 +0200

    Fix indent

--- plugins/twilighte_card/oric_twilighte_board_plugin.h
+++ plugins/twilighte_card/oric_twilighte_board_plugin.h
@@ -1,3 +1,3 @@
 
-struct  twilighte * twilighte_oric_init(void);
-unsigned char 	twilighteboard_oric_read(struct twilighte *twilighte, uint16_t addr);
+struct twilighte * twilighte_oric_init(void);
+unsigned char twilighteboard_oric_read(struct twilighte *twilighte, uint16_t addr);
