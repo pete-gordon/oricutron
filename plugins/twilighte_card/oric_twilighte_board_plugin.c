775ddd7da4e64e0fc4f0e07397089a468a67d4b6
jede <jede@oric.org>
2020-04-27 00:52:54 +0200

    Now rom are loaded

--- plugins/twilighte_card/oric_twilighte_board_plugin.c
+++ plugins/twilighte_card/oric_twilighte_board_plugin.c
@@ -16,8 +16,61 @@
 #include "../../system.h"
 
 extern SDL_bool read_config_string( char *buf, char *token, char *dest, Sint32 maxlen );
-
+///extern SDL_bool load_rom( struct machine *oric, char *fname, int size, unsigned char *where, struct symboltable *stab, int symflags );
  
+static SDL_bool load_rom_twilighte(  char *fname, int size, unsigned char *where )
+{
+  SDL_RWops *f;
+  char *tmpname;
+
+  // MinGW doesn't have asprintf :-(
+  tmpname = malloc( strlen( fname ) + 10 );
+  if( !tmpname ) return SDL_FALSE;
+
+  sprintf( tmpname, "%s.rom", fname );
+  f = SDL_RWFromFile( tmpname, "rb" );
+  if( !f )
+  {
+    error_printf( "Unable to open '%s'\n", tmpname );
+    free( tmpname );
+    return SDL_FALSE;
+  }
+
+  if( size < 0 )
+  {
+    int filesize;
+    SDL_RWseek( f, 0, SEEK_END );
+    filesize = SDL_RWtell( f );
+    SDL_RWseek( f, 0, SEEK_SET );
+
+    if( filesize > -size )
+    {
+      error_printf( "ROM '%s' exceeds %d bytes.\n", fname, -size );
+      SDL_RWclose( f );
+      free( tmpname );
+      return SDL_FALSE;
+    }
+
+    where += (-size)-filesize;
+    size = filesize;
+  }
+
+  if( SDL_RWread( f, where, size, 1 ) != 1 )
+  {
+    error_printf( "Unable to read '%s'\n", tmpname );
+    SDL_RWclose( f );
+    free( tmpname );
+    return SDL_FALSE;
+  }
+
+  SDL_RWclose( f );
+
+  sprintf( tmpname, "%s.sym", fname );
+  //mon_new_symbols( stab, oric, tmpname, symflags, SDL_FALSE, SDL_FALSE );
+  free( tmpname );
+
+  return SDL_TRUE;
+}
 
 struct twilbankinfo
 {
@@ -32,7 +85,7 @@ struct twilighte
   unsigned char t_banking_register;
   char twilrombankfiles[32][1024];
   char twilrombankdata[32][16834];
-  //struct twilbankinfo rom_bank[32];
+  struct twilbankinfo rom_bank[32];
   //struct twilbankinfo ram_bank[32];
 };
 
@@ -57,7 +110,7 @@ struct twilighte * twilighte_oric_init(void)
 		  return NULL;
 	}
 
-	for( j=1; j<31; j++ ) strcpy(twilighte->twilrombankfiles[j],"hello");
+	
 	while( !feof( f ) )
 	  {
 		  fgets( line, 1024, f );
@@ -78,6 +131,21 @@ struct twilighte * twilighte_oric_init(void)
 
   fclose( f );
 
+  // Now load rom 
+    for( i=1; i<31; i++ )
+      {
+
+//        oric->rom = oric->tele_bank[i].ptr  = &oric->mem[0x0c000+(i*0x4000)];
+        if( twilighte->twilrombankfiles[i][0] )
+        {
+          //oric->tele_bank[i].type = TELEBANK_ROM;
+          if( !load_rom_twilighte( twilighte->twilrombankfiles[i], -16384, &twilighte->rom_bank[i].ptr  ) ) return SDL_FALSE;
+          //load_patches( oric, telebankfiles[i] );
+        //} else {
+          //oric->tele_bank[i].type = TELEBANK_RAM;
+        //}
+      }
+	  }
 /* 
     
 */
