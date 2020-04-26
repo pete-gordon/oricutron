3fdc976eb5da437db62fe1b1da0c95ae632fcfb5
jede <jede@oric.org>
2020-04-27 00:35:09 +0200

    Now we can read filenames for banks

--- plugins/twilighte_card/oric_twilighte_board_plugin.c
+++ plugins/twilighte_card/oric_twilighte_board_plugin.c
@@ -2,6 +2,8 @@
 #define TWILIGHTE_CARD_ORIC_EXTENSION_REGISTER          0x342
 #define TWILIGHTE_CARD_ORIC_EXTENSION_BANKING_REGISTER  0x343
 
+
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -10,6 +12,12 @@
 #include <unistd.h>
 #include <string.h>
 
+
+#include "../../system.h"
+
+extern SDL_bool read_config_string( char *buf, char *token, char *dest, Sint32 maxlen );
+
+ 
 
 struct twilbankinfo
 {
@@ -22,17 +30,57 @@ struct twilighte
   
   unsigned char t_register;
   unsigned char t_banking_register;
-  struct twilbankinfo rom_bank[32];
-  struct twilbankinfo ram_bank[32];
+  char twilrombankfiles[32][1024];
+  char twilrombankdata[32][16834];
+  //struct twilbankinfo rom_bank[32];
+  //struct twilbankinfo ram_bank[32];
 };
+
+char twilrombankfiles[100][1024];
 
 struct twilighte * twilighte_oric_init(void)
 {
 	//struct twilighte * temp;
 	//temp=malloc(sizeof(struct twilighte *));
+	char twilrombankfiles[32][1024];
+	FILE *f;
+  	unsigned int i, j;
+	char tbtmp[32];
+	char line[1024];
 	struct twilighte *twilighte = malloc(sizeof(struct twilighte));
 	twilighte->t_banking_register=0;
 	twilighte->t_register=128+1;
+
+	f = fopen( "plugins/twilighte_card/twilighte.cfg", "r" );
+  	if( !f ) {
+		  error_printf( "plugins/twilighte_card/twilighte.cfg not found\n" );
+		  return NULL;
+	}
+
+	for( j=1; j<31; j++ ) strcpy(twilighte->twilrombankfiles[j],"hello");
+	while( !feof( f ) )
+	  {
+		  fgets( line, 1024, f );
+		  //error_printf( line );
+		  for( j=1; j<31; j++ )
+    	  {
+      		sprintf( tbtmp, "twilbankrom%c", j+'0' );
+      		if( read_config_string( line, tbtmp, twilighte->twilrombankfiles[j], 1024 ) ) 
+			  {
+				  //error_printf( line );
+				 error_printf("File: %d %s",j,twilighte->twilrombankfiles[j]);
+				 
+				  break;
+			  }
+			
+    	  }
+       }
+
+  fclose( f );
+
+/* 
+    
+*/
 	return  twilighte;
 }
 
